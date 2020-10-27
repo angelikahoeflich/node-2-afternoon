938526d6d729cb6fa45c12e196d6e6facfa5e532
Angelika Hoeflich <angelikahoeflich@Angelikas-MacBook-Air.local>
2020-10-27 13:32:06 -0700

    added module methods

--- server/controllers/messages_controller.js
+++ server/controllers/messages_controller.js
@@ -6,15 +6,18 @@ let currentId = 0;
 module.exports = {
 
   create: (req,res) => {
-    res.status(200).send
+    const { text, time} = req.body;
+    messages.push({id, text, time});
+    id++;
+    res.status(200).send(messages)
   },
-  read: () => {
+  read: (req, res) => { 
+      res.status(200).send(messages)
+  },
+  update: (req, res) => {
 
   },
-  update: () => {
-
-  },
-  delete: () => {
+  delete: (req, res) => {
 
   }
 
