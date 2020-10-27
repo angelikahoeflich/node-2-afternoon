805dfcaa414c150c38df73314b0dca445580162b
Angelika Hoeflich <angelikahoeflich@Angelikas-MacBook-Air.local>
2020-10-27 14:42:49 -0700

    it worked!

--- server/controllers/messages_controller.js
+++ server/controllers/messages_controller.js
@@ -7,18 +7,33 @@ module.exports = {
 
   create: (req,res) => {
     const { text, time} = req.body;
-    messages.push({id, text, time});
-    id++;
+    console.log("req.body", req.body)
+    messages.push({id: currentId, text, time});
+    currentId++;
     res.status(200).send(messages)
   },
   read: (req, res) => { 
       res.status(200).send(messages)
   },
+
   update: (req, res) => {
+    const { text } = req.body;
+    const updateID = req.params.id;
+    const messageIndex = messages.findIndex( el => el.id == updateID);
 
+    messages[messageIndex] = {
+      id: messages[messageIndex].id,
+      text: text || messages[messageIndex].text,
+      time: messages[messageIndex].time
+    };
+    res.status(200).send(messages)
   },
-  delete: (req, res) => {
 
+  delete: (req, res) => {
+      const deleteId = req.params.id;
+      const messageIndex = messages.findIndex( el => el.id == deleteId);
+      messages.splice(messageIndex, 1);
+      res.status(200).send(messages);
   }
 
 }
--- server/index.js
+++ server/index.js
@@ -1,7 +1,15 @@
 const express = require('express');
 const app = express();
+const mc = require('./controllers/messages_controller');
 
 app.use(express.json());
+app.use(express.static(__dirname + '/../public/build'))
+
+app.post('/api/messages', mc.create)
+app.get('/api/messages', mc.read)
+app.put('/api/messages/:id', mc.update)
+app.delete('/api/messages/:id', mc.delete)
+
 
 app.listen(3001, console.log('listening on port 3001'));
 
