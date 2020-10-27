fd62cefe38a24a84a79c2786f889a73461c3b001
Angelika Hoeflich <angelikahoeflich@Angelikas-MacBook-Air.local>
2020-10-27 12:54:54 -0700

    added controller folder

---
+++ server/controllers/messages_controller.js
@@ -0,0 +1,25 @@
+let messages = [];
+
+let currentId = 0;
+
+
+module.exports = {
+
+  create: (req,res) => {
+    res.status(200).send
+  },
+  read: () => {
+
+  },
+  update: () => {
+
+  },
+  delete: () => {
+
+  }
+
+}
+
+
+
+
